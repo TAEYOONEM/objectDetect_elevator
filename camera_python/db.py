09e442ba0b62f9136574c5f43f20e47ed6bc8027
YunBeomOh <95226098+TaeYoonSS@users.noreply.github.com>
2023-08-08 10:02:11 +0900

    update

--- camera_python/db.py
+++ camera_python/db.py
@@ -16,35 +16,37 @@ class ResidentInfo :
         except Exception as e :
             print(f'Can\'t connect  db : {e}')
 
-    def execute(self,list_of_ids) :
-        # lst = ['Warning']
-        format_strings = ','.join(['%s'] * len(lst))
-        self.cur.execute("SELECT floor FROM floors WHERE name in (%s)" % format_strings, tuple(list_of_ids))
-        # self.cur.execute(query, tuple(list_of_ids))
+    def get_query(self, list_of_ids) :        
+        format_strings = ','.join(['%s'] * len(list_of_ids))
+        query = "SELECT floor FROM floors WHERE name in (%s)" % format_strings
+        return query
+    
+    def execute(self, *args) :
+        try :
+            query = args[0]
+            list_of_ids = args[1]
+            self.cur.execute(query, tuple(list_of_ids))
+        except IndexError :
+            self.cur.execute(query)
 
     def fetchone(self) :
         try :
             return self.cur.fetchone()
         except Exception as e :
             print(f'{e}')
-    
-    def get_query(self) :        
-        format_strings = ','.join(['%s'] * len(lst))
-        query = f"SELECT floor FROM floors WHERE name in {format_strings}" 
-        return query
-    
+        
     def commit(self) :
         self.conn.commit()
 
     def close(self) :
         self.conn.close()
 
-
 if __name__ == "__main__" :
-    lst = ['Warning']
+    list_of_ids = ['Warning']
     db = ResidentInfo(user = 'root', passwd='12345', host='localhost', db='resident')
-    # q= db.get_query()
-    db.execute(lst)
+    q = db.get_query(list_of_ids)
+    # q = "SELECT floor FROM floors WHERE name = 'Warning'"
+    db.execute(q, list_of_ids)
     print(db.fetchone())
 
 
